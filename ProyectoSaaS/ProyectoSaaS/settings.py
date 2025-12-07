f305e9aafc6b7553d976c7389d9d0bd67f6adfdf
Anghelo Bustamante Torres <anghelo.bustamante97@gmail.com>
2025-12-07 02:17:51 -0300

    cambio db y subido aws

--- ProyectoSaaS/ProyectoSaaS/settings.py
+++ ProyectoSaaS/ProyectoSaaS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=e^4*9ay&g_cpdj-b@svda5z@k#owwfzvtwv*r941lf!ue=sh8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.218.195.175', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ProyectoSaaS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mipassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
